ca6df6dc3570c92d07bcbf47b37c8b7691bcc86d
writemorecode <gustav.ae.karlsson@gmail.com>
2016-01-23 16:49:56 +0100

    Updated setup.sh and changed colorscheme in vimrc

--- setup.sh
+++ setup.sh
@@ -6,4 +6,12 @@ echo "Created Neovim directories"
 ln -s /home/gustav/dotfiles/.vimrc /home/gustav/.config/nvim/init.vim
 echo "Symlinked .vimrc in Git repo to init.vim in .config/nvim"
 
+curl -sfLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+echo "vim-plug installed"
+
+git clone --quiet https://github.com/tomasr/molokai
+cp molokai/colors/molokai.vim .config/nvim/colors
+echo "Molokai colorscheme downloaded and installed"
+
 echo "All done."
