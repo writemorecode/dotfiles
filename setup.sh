54ee781b1bb3025f9d18a19efb4469ce6f0a89c2
writemorecode <gustav.ae.karlsson@gmail.com>
2016-05-09 20:20:52 +0200

    Fixed script, it should work now.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 mkdir -p ~/.config/nvim/colors
 echo "Created Neovim directories"
 
-cp /home/gustav/dotfiles/.vimrc /home/gustav/.config/nvim/init.vim
-echo "Copied .vimrc in Git repo to init.vim in .config/nvim"
+cp /home/gustav/dotfiles/init.vim /home/gustav/.config/nvim/init.vim
+echo "Copied init.vim in Git repo to init.vim in .config/nvim"
 
 curl -sfLo ~/.config/nvim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
@@ -12,7 +12,6 @@ echo "vim-plug installed"
 
 git clone --quiet https://github.com/tomasr/molokai
 cp molokai/colors/molokai.vim .config/nvim/colors
-rm -rf molokai
 echo "Molokai colorscheme downloaded and installed"
 
 echo "All done."
